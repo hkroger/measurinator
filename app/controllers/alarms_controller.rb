4242ff3550ef979fa689d65324ab97abc40e71f5
Hannu Kroger <hkroger@gmail.com>
2021-10-03 18:17:06 +0300

    Add null check

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -1,12 +1,11 @@
 # -*- encoding : utf-8 -*-
 
 class AlarmsController < AuthorizedController
-
   def index
-    @alarms = Alarm.all
-    @clients = @alarms.map{|l| l.location.client_id}.uniq.map do |client_id|
-       Client.find_by_id(client_id)
-    end.sort_by{ |c| c.name }
+    @alarms  = Alarm.all
+    @clients = @alarms.map { |l| l.location.client_id }.uniq.map do |client_id|
+      Client.find_by_id(client_id)
+    end.select { |c| !c.nil? }.sort_by { |c| c.name }
   end
 
   def new
@@ -31,30 +30,29 @@ class AlarmsController < AuthorizedController
       return redirect_to alarms_path, :notice => "Invalid log search parameters"
     end
 
-    @clients = @logs.map{|l| l.client_id}.uniq.map do |client_id|
-       Client.find_by_id(client_id)
+    @clients      = @logs.map { |l| l.client_id }.uniq.map do |client_id|
+      Client.find_by_id(client_id)
     end
     @client_names = {}
     @clients.each do |c|
       @client_names[c.id] = c.name
     end
 
-
   end
-  
+
   def create
     if params[:id].present?
       a = Alarm.find_by_location_id_and_id(params[:location], params[:id])
     else
-      a = Alarm.new
+      a       = Alarm.new
       a.owner = current_user.username
     end
 
-    a.location_id = params[:location].to_i
-    a.temperature = params[:threshold].to_f
-    a.hysteresis = params[:hysteresis].to_f.abs
+    a.location_id       = params[:location].to_i
+    a.temperature       = params[:threshold].to_f
+    a.hysteresis        = params[:hysteresis].to_f.abs
     a.alarm_when_warmer = params[:alarm_when_warmer].to_i == 1
-    a.two_way_alarm = params[:two_way_alarm].present?
+    a.two_way_alarm     = params[:two_way_alarm].present?
     a.save
 
     redirect_to alarms_path, :notice => params[:id] ? "Alarm has been updated." : "Alarm has been created."
@@ -65,15 +63,15 @@ class AlarmsController < AuthorizedController
   end
 
   def toggle
-    raise "Wut?" unless %w(email pushover).include? params[:notification] 
-    raise "Wut2?" unless %w(true false).include? params[:value] 
-    value =  params['value'] == 'true'
+    raise "Wut?" unless %w(email pushover).include? params[:notification]
+    raise "Wut2?" unless %w(true false).include? params[:value]
+    value = params['value'] == 'true'
     alarm = Cassandra::TimeUuid.new(params[:id])
-    an = AlarmNotification.find_by_alarm_and_user(alarm, current_user.username)
+    an    = AlarmNotification.find_by_alarm_and_user(alarm, current_user.username)
     if an.nil?
-      an = AlarmNotification.new(:alarm_id => alarm,
+      an = AlarmNotification.new(:alarm_id              => alarm,
                                  params['notification'] => value,
-                                 :username => current_user.username)
+                                 :username              => current_user.username)
       an.save
     else
       an.send(params['notification'] + "=", value)
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,11 @@
 # -*- encoding : utf-8 -*-
+
 class LocationsController < AuthorizedController
   def index
-    @locations = Location.all.select { |l| current_user.admin? || l.username == current_user.username }.sort_by{ |i| i.description }
-    @clients = @locations.map{|l| l.client_id}.uniq.map do |client_id|
-       Client.find_by_id(client_id)
-    end.sort_by{ |c| c.name }
+    @locations = Location.all.select { |l| current_user.admin? || l.username == current_user.username }.sort_by { |i| i.description }
+    @clients   = @locations.map { |l| l.client_id }.uniq.map do |client_id|
+      Client.find_by_id(client_id)
+    end.select { |c| !c.nil? }.sort_by { |c| c.name }
   end
 
   def new
@@ -14,28 +15,28 @@ class LocationsController < AuthorizedController
   def edit
     @location = Location.find_by_id(params[:id])
   end
-  
+
   def create
     if not params[:old_id].blank?
-      a = Location.find_by_id(params[:old_id])
+      a          = Location.find_by_id(params[:old_id])
       a.username = params[:username]
     else
       a = Location.new
       begin
-        a.id = Random.rand(100000000)
+        a.id              = Random.rand(100000000)
         existing_location = Location.find_by_id(a.id)
       end while !existing_location.nil?
-      
+
       a.username = current_user.username
     end
 
-    a.description = params[:name]
-    a.quantity = params[:quantity].to_sym
-    a.client_id = Cassandra::Uuid.new(params[:client_id])
-    a.do_not_alarm = params[:do_not_alarm].present?
-    a.do_not_show = params[:do_not_show].present?
+    a.description            = params[:name]
+    a.quantity               = params[:quantity].to_sym
+    a.client_id              = Cassandra::Uuid.new(params[:client_id])
+    a.do_not_alarm           = params[:do_not_alarm].present?
+    a.do_not_show            = params[:do_not_show].present?
     a.do_not_show_publically = params[:do_not_show_publically].present?
-    a.sensors = Set.new(params[:sensor_list].gsub(" ","").split(",").map(&:to_i))
+    a.sensors                = Set.new(params[:sensor_list].gsub(" ", "").split(",").map(&:to_i))
 
     a.sensors.each do |sensor|
       s = Sensor.find_by_id(sensor)
@@ -54,15 +55,15 @@ class LocationsController < AuthorizedController
   end
 
   def toggle
-    raise "Wut?" unless %w(email pushover).include? params[:notification] 
-    raise "Wut2?" unless %w(true false).include? params[:value] 
-    value =  params['value'] == 'true'
+    raise "Wut?" unless %w(email pushover).include? params[:notification]
+    raise "Wut2?" unless %w(true false).include? params[:value]
+    value    = params['value'] == 'true'
     location = Cassandra::TimeUuid.new(params[:id])
-    an = LocationNotification.find_by_location_and_user(location, current_user.username)
+    an       = LocationNotification.find_by_location_and_user(location, current_user.username)
     if an.nil?
-      an = LocationNotification.new(:location_id => location,
-                                 params['notification'] => value,
-                                 :username => current_user.username)
+      an = LocationNotification.new(:location_id           => location,
+                                    params['notification'] => value,
+                                    :username              => current_user.username)
       an.save
     else
       an.send(params['notification'] + "=", value)
